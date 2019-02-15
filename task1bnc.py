ea2a495bf4ee86071a545192cc7d20a97c5f50ca
karandeep Sethi <singhkd60@gmail.com>
2019-02-15 10:17:47 -0800

    part 2 and 3 is done

---
+++ task1bnc.py
@@ -0,0 +1,24 @@
+
+def count_the_article(d):
+	for line in fin1,fin2,fin3:
+
+		return (list(d.values()))
+
+
+
+def sorted_words(d):
+	t=[]
+	for key,values in d.items():
+		t.append(value,keys)
+		t.sort(reverse==true)
+	return t
+
+
+fin1=open("Book1.txt")
+fin2=open("Book2.txt")
+fin3=open("Book3.txt")
+d=dict()
+
+
+print(count_the_article(d))
+print(sorted_words(d))
